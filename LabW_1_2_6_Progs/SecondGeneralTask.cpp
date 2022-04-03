54830ae8228c853c36e5eea166975c83f985a185
Александр Немеров <nemerov_a@mail.ru>
2022-04-03 22:46:57 +0800

    ytytjjf

--- LabW_1_2_6_Progs/SecondGeneralTask.cpp
+++ LabW_1_2_6_Progs/SecondGeneralTask.cpp
@@ -1,5 +1,4 @@
 #include "SecondGeneralTask.h"
-#include "ThisIs.h"
 
 #include "Libraries.h"
 
@@ -21,9 +20,22 @@ void secondGeneralTask()
 {
 	cout << "\n\t1 - Добавить журнал\n\t2 - Добавить книгу\n\t3 - Добавить учебник";
 	cout << "\n\t4 - Вывод всех журналов\n\t5 - Вывод всех книг\n\t6 - Вывод всех учебников\n\t";
-	char kIn
+	char kIn;
+	cin >> kIn;
 
-	
+	int inputInt;
+	try
+	{
+		inputInt = isInteger(kIn);
+	}
+	catch (const exception& err)
+	{
+		cout << "\n\t" << err.what() << "... ";
+		char p = _getch();
+		system("cls");
+		return;
+	}
+
 
 
 }
